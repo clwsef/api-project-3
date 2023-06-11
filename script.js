88b902f33a5e2e0849fdd8c1578018e42bfa329b
clwsef <135839448+clwsef@users.noreply.github.com>
2023-06-12 02:03:13 +0600

    цвет темы под покемона

--- script.js
+++ script.js
@@ -36,3 +36,46 @@ const typeColor = {
     const statAttack = data.stats[1].base_stat
     const statDefense = data.stats[2].base_stat
     const statSpeed = data.stats[5].base_stat
+    const themeColor = typeColor[data.types[0].type.name]
+    console.log(themeColor)
+    card.innerHTML = `
+          <p class="hp">
+            <span>HP</span>
+              ${hp}
+          </p>
+          <img src=${imgSrc} />
+          <h2 class="poke-name">${pokeName}</h2>
+          <div class="types">
+           
+          </div>
+          <div class="stats">
+            <div>
+              <h3>${statAttack}</h3>
+              <p>Attack</p>
+            </div>
+            <div>
+              <h3>${statDefense}</h3>
+              <p>Defense</p>
+            </div>
+            <div>
+              <h3>${statSpeed}</h3>
+              <p>Speed</p>
+            </div>
+          </div>
+    `
+    appendTypes(data.types)
+    styleCard(themeColor)
+  }
+  let appendTypes = (types) => {
+    types.forEach((item) => {
+      let span = document.createElement("SPAN")
+      span.textContent = item.type.name
+      document.querySelector(".types").appendChild(span)
+    })
+  }
+  let styleCard = (color) => {
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`
+    card.querySelectorAll(".types span").forEach((typeColor) => {
+      typeColor.style.backgroundColor = color
+    });
+  };
